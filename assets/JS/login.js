6d8be90f33fe10cf2e76fa200a315070c123d964
u_chiha <owayopaul@gmail.com>
2024-03-23 10:17:33 +0300

    LuKoBa

--- assets/JS/login.js
+++ assets/JS/login.js
@@ -4,12 +4,14 @@ let nameField = document.getElementById("nameField");
 let title = document.getElementById("title");
 
 loginBtn.onclick = function(){
+    window.open('login.html', '_self');
     nameField.style.maxHeight = "0";
     title.innerHTML = "Login"
     registerBtn.classList.add("disable");
     loginBtn.classList.remove("disable");
 }
 registerBtn.onclick = function(){
+    window.open('login.html', '_self');
     nameField.style.maxHeight = "60px";
     title.innerHTML = "Register"
     loginBtn.classList.add("disable");
